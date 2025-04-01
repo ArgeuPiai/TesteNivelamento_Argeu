f0a68687e0db54d328ebfb180ab1e5b74a259920
ArgeuPiai <argeu.piai@gmail.com>
2025-03-31 21:34:27 -0300

    v 1.0 teste 1 - Teste concluido com sucesso! (se der tempo adicionar mais interatividade e trocar por funçoes)

--- Teste_1/src/teste1.py
+++ Teste_1/src/teste1.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import zipfile
 
 # 1. receber o site
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
@@ -23,9 +24,19 @@ for link in soup.find_all('a'):
             nome_arquivo = os.path.basename(href)
             pdf_links.append(('Anexo_II', link['href']))
 
+# 3. Salvar PDFs e compactar
+with zipfile.ZipFile('../data/output/Anexos_ANS.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    for nome_arquivo, url_pdf in pdf_links:
+        try:
+            print(f"Baixando {nome_arquivo}...")
+            pdf_data = requests.get(url_pdf, timeout=10).content
+            caminho = os.path.join('../data/input', nome_arquivo)
 
-# 3. salvar PDFs e compactar
-for nome_arquivo, url_pdf in pdf_links:
-    print(f"Baixando {nome_arquivo}...")
+            with open(caminho, 'wb') as f:
+                f.write(pdf_data)
+            zipf.write(caminho, nome_arquivo)
+            print(f"{nome_arquivo} baixado com sucesso!")
+        except Exception as e:
+            print(f"Erro ao baixar {nome_arquivo}: {e}")
 
-print("\n✅ baixados e compactados!")
+print("\n✅ Todos os PDFs foram baixados e compactados!")
